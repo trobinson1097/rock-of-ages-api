4aed2f76f0595fe1d1b96d79545b9e8a0bd50c01
Valerie Freeman <valerah7@gmail.com>
2025-04-30 15:50:14 -0500

    containerzzzzz

--- seed_database.sh
+++ seed_database.sh
@@ -1,9 +1,11 @@
-   #!/bin/bash
+#!/bin/bash
 
-   rm db.sqlite3
-   rm -rf ./rockapi/migrations
-   python3 manage.py migrate
-   python3 manage.py makemigrations rockapi
-   python3 manage.py migrate rockapi
-   python3 manage.py loaddata types
-   python3 manage.py loaddata rocks
+rm db.sqlite3
+rm -rf ./rockapi/migrations
+python3 manage.py migrate
+python3 manage.py makemigrations rockapi
+python3 manage.py migrate rockapi
+python3 manage.py loaddata users
+python3 manage.py loaddata tokens
+python3 manage.py loaddata types
+python3 manage.py loaddata rocks
